85d6410682d44ffea072e7244ce878f122fe99ce
Sergei Maertens <sergei@maykinmedia.nl>
2020-09-17 15:54:38 +0200

    :alembic: Specify worker name & queue on CLI

--- bin/celery_worker.sh
+++ bin/celery_worker.sh
@@ -2,14 +2,18 @@
 
 set -e
 
+queue=${1:-celery}
+name=${2:-worker.$queue}
+
 LOGLEVEL=${CELERY_LOGLEVEL:-INFO}
 CONCURRENCY=${CELERY_WORKER_CONCURRENCY:-1}
 
 echo "Starting celery worker"
 celery worker \
     --app bptl \
+    -n $name \
     -l $LOGLEVEL \
     --workdir src \
     -O fair \
     -c $CONCURRENCY \
-    -Q celery,foo,bar
+    -Q $queue
